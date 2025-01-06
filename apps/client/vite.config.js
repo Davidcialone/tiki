7018fad2d0af9d35df16314b6912c13e310c1abc
Davidcialone <cialonedavid@gmail.com>
2025-01-06 10:34:41 +0100

    update vite.config

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,36 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ["sequelize", "pg-hstore"],
   },
-  build: {
-    outDir: "dist",
-    // Ajout de la configuration du build
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  // Configuration du serveur
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-  },
-  // Configuration des variables d'environnement
   define: {
     "process.env.VITE_API_BASE_URL": JSON.stringify(
       "https://tiki-ermployee.vercel.app"
     ),
-  },
-  // Ajout de la résolution des modules
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
   },
 });
