e6fe8dc56a09211c5778bf4b04712fceb8879a15
Davidcialone <cialonedavid@gmail.com>
2025-01-07 14:48:22 +0100

    fix create reservation

--- apps/client/src/api/reservationApi.js
+++ apps/client/src/api/reservationApi.js
@@ -1,5 +1,5 @@
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
-
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+// || "http://localhost:5000";
 /**
  * Fonction utilitaire pour les requêtes fetch
  */
