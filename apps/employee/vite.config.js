7758486b134de85b9902c6239439c9513ea29674
Davidcialone <cialonedavid@gmail.com>
2025-01-24 17:42:09 +0100

    add build command in vercel.json 5

--- apps/employee/vite.config.js
+++ apps/employee/vite.config.js
@@ -1,36 +1,13 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import path from "path";
 
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["sequelize", "pg-hstore"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist", // Chemin de sortie pour les fichiers build
-    rollupOptions: {
-      external: ["react", "react-dom"],
-    },
-  },
-  server: {
-    port: 5174,
-    proxy: {
-      "/api": {
-        target: "https://tiki-ew5j.onrender.com",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
   },
   define: {
     "process.env.VITE_API_BASE_URL": JSON.stringify(
